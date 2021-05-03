c1cfc4178305aed33432786f45b38824c074073f
Erik Beerepoot 😊 <erikbeerepoot@hush.com>
2021-05-03 08:03:52 -0700

    Fix up encode/decode, use in step, add calls to gymclient

--- notebooks/fusion_gym_environment.py
+++ notebooks/fusion_gym_environment.py
@@ -49,19 +49,29 @@ class ExtrudeAction:
     end_face: uuid4
     # the type of operation (defined above)
     operation: Operation
-        
-    def encode_action(self) -> np.array:
-        start_lower_64 = start_face.int & ((2 ** 64) - 1)
-        start_upper_64 = start_face.int >> 64 & ((2 ** 64) - 1)
-        end_lower_64 = start_face.int & ((2 ** 64) - 1)
-        end_upper_64 = start_face.int >> 64 & ((2 ** 64) - 1)
 
-        return np.array(
+    def encode(self) -> np.array:
+        """ Encodes an action to tf-agent compatible format"""
+        start_lower_64 = self.start_face.int & ((2 ** 64) - 1)
+        start_upper_64 = self.start_face.int >> 64 & ((2 ** 64) - 1)
+        end_lower_64 = self.end_face.int & ((2 ** 64) - 1)
+        end_upper_64 = self.end_face.int >> 64 & ((2 ** 64) - 1)
+
+        return np.array([
             start_upper_64,
             start_lower_64,
             end_upper_64,
             end_lower_64,
-            int(operation)
+            int(self.operation)
+        ], dtype=np.uint64)
+
+    def decode(encoded: np.array) -> ExtrudeAction:
+        start_face = (int(encoded[0]) << 64) | encoded[1]
+        end_face   = (int(encoded[2]) << 64) | encoded[3]
+        return ExtrudeAction(
+            uuid.UUID(int=start_face),
+            uuid.UUID(int=end_face),
+            Operation(encoded[4])
         )
     
 @dataclass(frozen=True)
@@ -69,10 +79,10 @@ class State:
     step: int
     finished: bool
 
+
 class FusionGymEnvironment(py_environment.PyEnvironment):
         
-    def __init__(self, gym_env: GymEnv, max_steps):
-        # Action => array()
+    def __init__(self, gym_env: FusionEnvWrapper, max_steps):
         self._action_spec = array_spec.BoundedArraySpec(
             shape=(5,), dtype=np.int64, name='action')
         self._observation_spec = array_spec.BoundedArraySpec(
@@ -80,7 +90,7 @@ class FusionGymEnvironment(py_environment.PyEnvironment):
         self._state = State(0, False)
         self._episode_ended = False
                 
-        self._gym_env = gym_env        
+        self._env = gym_env
         self._max_steps = max_steps
         
     def action_spec(self):
@@ -91,13 +101,28 @@ class FusionGymEnvironment(py_environment.PyEnvironment):
 
     def _reset(self):
         self._state = State(0, False)
+        self._env.revert_to_target()
         return ts.restart(np.array([self._state.step], dtype=np.int32))
 
     def _step(self, action):
         self._state = State(self._state.step + 1, self._state.step >= self._max_steps)
         print(f"step: {self._state}")
         
-   
+        # Apply action
+        try:
+            decoded_action = ExtrudeAction.decode(action)
+            print(f"action: {decoded_action}")
+            graph, iou = self._env.extrude(
+                decoded_action.start_face,
+                decoded_action.end_face,
+                decoded_action.operation
+            )
+            print(graph)
+        except ValueError:
+            #print("Could not decode action")
+            pass
+
+
         if self._state.finished:
             return ts.termination(np.array([self._state.step], dtype=np.int32), 0.0)
         else:
